622fae7de1839e707b0f8f84fce9508518112c48
toor0019 <toor0019@algonquinlive.com>
2018-11-13 22:27:09 -0500

    Intial Commit

---
+++ app/src/main/java/com/example/gurki/cst2335_finalproject/FoodNutritionActivity.java
@@ -0,0 +1,13 @@
+package com.example.gurki.cst2335_finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodNutritionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_nutrition);
+    }
+}
---
+++ app/src/main/java/com/example/gurki/cst2335_finalproject/StartActivity.java
@@ -0,0 +1,61 @@
+package com.example.gurki.cst2335_finalproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+
+public class StartActivity extends AppCompatActivity {
+
+
+    private Button m_OC_Transpo;
+    private Button m_Movie_Information;
+    private Button m_CBC_News_Reader;
+    private Button m_Food_Nutrition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        m_OC_Transpo= (Button)findViewById(R.id.oc_transpo_bus_route);
+        m_Movie_Information=(Button)findViewById(R.id.movie_information);
+        m_CBC_News_Reader=(Button)findViewById(R.id.cbc_news_reader);
+        m_Food_Nutrition=(Button)findViewById(R.id.food_nutrition);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        m_OC_Transpo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,OCTranspoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        m_Movie_Information.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,MovieInformationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        m_CBC_News_Reader.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,CBCNewsReaderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        m_Food_Nutrition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this,FoodNutritionActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
